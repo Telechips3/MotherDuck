1a13d5717e8bddf4796b7b2c88c0d9867390f092
ssamKim0908 <mmmek0401@gmail.com>
2026-02-02 10:18:55 +0900

    init mother vcp

---
+++ mother/vcp/sources/app.sample/app.base/Team2/source/spi.c
@@ -0,0 +1,45 @@
+#include "spi.h"
+#include "speed.h"
+
+static volatile uint32_t spi_rx_buf[1] = {0};
+static uint32_t spi_tx_buf[1] = {0};
+
+static void spi_receive(uint32 uiCh, uint32 iEvent, void *pArg)
+{
+    mcu_printf("[SPI] PIO Received: %d\n", spi_rx_buf[0]);
+    control_motor_drive(spi_rx_buf[0]);
+    GPSB_AsyncXfer(SPI_CHANNEL, (uint32 *)spi_tx_buf, (uint32 *)spi_rx_buf, 1, 
+                   GPSB_XFER_MODE_WITH_INTERRUPT | GPSB_XFER_MODE_WITHOUT_CTF);
+}
+
+void SPI_Init(void)
+{
+    GPIO_Config(SPI_CS_GPIO, GPIO_FUNC(0) | GPIO_INPUT | GPIO_INPUTBUF_EN | GPIO_PULLUP);
+    GPIO_Set(SPI_CS_GPIO, 1);
+
+    // GPIO_Config(SPI_SCLK_GPIO, GPIO_FUNC(SPI_GPIO_FUNC) | GPIO_INPUT | GPIO_INPUTBUF_EN | GPIO_PULLUP);
+    // GPIO_Config(SPI_MOSI_GPIO, GPIO_FUNC(SPI_GPIO_FUNC) | GPIO_INPUT | GPIO_INPUTBUF_EN | GPIO_PULLUP);
+    // GPIO_Config(SPI_MISO_GPIO, GPIO_FUNC(SPI_GPIO_FUNC) | GPIO_OUTPUT);
+
+    GPSBOpenParam_t param = {
+        .uiSdo = SPI_MOSI_GPIO,
+        .uiSdi = SPI_MISO_GPIO,
+        .uiSclk = SPI_SCLK_GPIO,
+        .uiIsSlave = GPSB_SLAVE_MODE,
+        .uiDmaBufSize = 0,
+        .pDmaAddrTx = NULL,
+        .pDmaAddrRx = NULL,
+        .fbCallback = (GPSBCallback)(spi_receive),
+        .pArg = NULL};
+
+    if (GPSB_Open(SPI_CHANNEL, param) != SAL_RET_SUCCESS)
+    {
+        mcu_printf("[SPI] GPSB open failed\n");
+        return;
+    }
+
+    GPSB_Init();
+    GPSB_SetBpw(SPI_CHANNEL, 8);
+    GPSB_AsyncXfer(SPI_CHANNEL, (uint32 *)spi_tx_buf, (uint32 *)spi_rx_buf, 1,
+                   GPSB_XFER_MODE_WITH_INTERRUPT | GPSB_XFER_MODE_WITHOUT_CTF);
+}
---
+++ mother/vcp/sources/app.sample/app.base/rules.mk
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: Apache-2.0
+
+###################################################################################################
+#
+#   FileName : ruls.mk
+#
+#   Copyright (c) Telechips Inc.
+#
+#   Description :
+#
+#
+###################################################################################################
+
+MCU_BSP_APP_SAMPLE_BASE_PATH := $(MCU_BSP_BUILD_CURDIR)
+
+# Flags
+COMMON_FLAGS += -DMCU_BSP_SUPPORT_APP_BASE=1
+
+# Paths
+VPATH += $(MCU_BSP_APP_SAMPLE_BASE_PATH)
+
+# Includes
+INCLUDES += -I$(MCU_BSP_APP_SAMPLE_BASE_PATH)
+INCLUDES += -I$(MCU_BSP_APP_SAMPLE_BASE_PATH)/$(MCU_BSP_CHIPSET_FAMILY_NAME)
+
+# Sources
+SRCS += main.c
+
+# Team2 file
+TEAM2_ROOT_PATH := $(MCU_BSP_APP_SAMPLE_BASE_PATH)/Team2
+TEAM2_INC_PATH  := $(TEAM2_ROOT_PATH)/include
+TEAM2_SRC_PATH  := $(TEAM2_ROOT_PATH)/source
+
+INT_PATH := ../test.app.gpio/
+
+VPATH += $(TEAM2_SRC_PATH)
+
+INCLUDES += -I$(TEAM2_ROOT_PATH)
+INCLUDES += -I$(TEAM2_INC_PATH)
+# INCLUDES += -I$(INT_PATH)          # 향후 include 폴더에 넣을 헤더 대비
+
+SRCS += speed.c
+SRCS += interrupt_example.c
+SRCS += encoder.c
+SRCS += spi.c
+# SRCS += gpio_test.c
