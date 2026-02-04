4dd40cb7366feff0f800871d8f4094919b67b6c1
yoonsuhcho <yscho789@naver.com>
2026-02-04 20:44:55 +0900

    Fix encoder tick calculation

--- baby/vcp/sources/app.sample/app.base/Team2/source/encoder.c
+++ baby/vcp/sources/app.sample/app.base/Team2/source/encoder.c
@@ -34,11 +34,17 @@ static uint32 s_lastTransMs = 0;
 static uint8 s_prevA = 0;
 static uint32 s_lastCountMs = 0;
 
+// static inline uint32 get_tick_ms(void)
+// {
+//     uint32 ret;
+//     (uint32)(SAL_GetTickCount(&ret) * portTICK_PERIOD_MS);
+//     return ret;
+// }
 static inline uint32 get_tick_ms(void)
 {
-    uint32 ret;
-    (uint32)(SAL_GetTickCount(&ret) * portTICK_PERIOD_MS);
-    return ret;
+    uint32 tick = 0;
+    (void)SAL_GetTickCount(&tick);
+    return tick * portTICK_PERIOD_MS;
 }
 
 /* ===== Init ===== */
