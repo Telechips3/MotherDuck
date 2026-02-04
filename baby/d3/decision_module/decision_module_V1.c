e5607b3b85aa9e1d3405871bfc1fc7eb4655cba3
0731chae <0731chae@gmail.com>
2026-02-04 09:02:52 +0900

    minor fix

--- baby/d3/decision_module/decision_module_V1.c
+++ baby/d3/decision_module/decision_module_V1.c
@@ -2,34 +2,21 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <inttypes.h>
 
 #include "decide_mode.h"
 
-// ====== reason code (debug) =====
-typedef enum {
-    REASON_NONE = 0,
-    REASON_CPU_STALE = 1,
-    REASON_LEADER_ESTOP = 2,
-    REASON_LEADER_FAULT = 3,
-    REASON_WP_TIMEOUT = 4,
-    REASON_ARUCO_TIMEOUT = 5,
-    REASON_WP_RECOVERED = 6,
-    REASON_ARUCO_RECOVERED = 7,
-    REASON_BOTH_LOST = 8,
-    REASON_INIT = 9,
-    REASON_TRANSMITION = 10,
-    REASON_NULL = 11
-} mode_reason_t;
 
 // ===== Payload msg ==============
 to_vcp_msg_t msg = {0};
-
 
 void init_decide_mode_msg(void) {
     memset(&msg, 0, sizeof(msg));
 }
 
-uint64_t GetTickCount() 
+uint32_t GetTickCount(void) 
 {
     struct timespec ts;
     
@@ -41,29 +28,72 @@ uint64_t GetTickCount()
     }
 
     // 초(s) * 1000 + 나노초(ns) / 1,000,000 = 밀리초(ms)
-    return (uint64_t)ts.tv_sec * 1000 + (ts.tv_nsec / 1000000);
+    uint64_t ms = (uint64_t)ts.tv_sec * 1000ull + (uint64_t)(ts.tv_nsec / 1000000ull);
+    return (uint32_t)ms; // 여기서만 32비트로 잘라서 roll-over 자연스럽게
 }
 
+uint8_t RX_waypoint(to_vcp_msg_t *msg){
+
+    return 0;
+}
+
+uint8_t RX_aruco_marker(to_vcp_msg_t *msg){
+
+
+    return 0;
+}
+
+
+
 int main(void){
-    uint8_t seq_cnt = 0;
+    uint32_t seq = 0;
+    init_decide_mode_msg();
+    static uint32_t last_aruco_rx_time;
+    static uint32_t last_wp_rx_time;
 
     while(1){
+    uint8_t ret = 0;        // Handler
+    uint32_t time_now = 0;
+    
+    time_now = GetTickCount();
 
-    init_decide_mode_msg();
-
-    msg.seq = seq_cnt++;
+    msg.cpu_time_ms = time_now;
+    msg.seq = seq++;
+    
     //aruco marker input
+    ret = RX_aruco_marker(&msg);
+    if(ret){
+        fprintf(stderr,"[seq : %u| time : %u] Failed to receive aruco marker data\n",msg.seq, time_now);
+    }
+
+    if(!ret){
+        msg.aruco_valid = !ret;
+        last_aruco_rx_time = time_now;
+    }
+    msg.aruco_age_ms = (uint16_t)(time_now - last_aruco_rx_time);   // timeout check , 16bit casting
     
     // waypoint input
+    ret = RX_waypoint(&msg);
+    if(ret){
+        fprintf(stderr,"[seq : %u| time : %u] Failed to receive waypoint data\n",msg.seq, time_now);
+    }
+    if(!ret){
+        msg.wp_valid = !ret;
+        last_wp_rx_time = time_now;
+    }
+    msg.wp_age_ms = (uint16_t)(time_now - last_wp_rx_time);         // timeout check , 16bit casting
 
+    ret = decide_mode_step(&msg);
+
+    time_now = GetTickCount();
+    printf("[seq : %u| time : %u] mode=%u reason=%u leader_state : %u\n", 
+            msg.seq, time_now, msg.mode, msg.reason, msg.leader_state);
+    printf("[ArUco status] aruco_valid : %u, aruco_age_ms : %u, aruco_dist_mm : %d, aruco_x_norm_q15 : %d",
+          msg.aruco_valid, msg.aruco_age_ms, msg.aruco_dist_mm, msg.aruco_x_norm_q15);
+    printf("[Waypoint status] wp_valid: %u, wp_age_ms : %u, leader_x_mm : %d, leader_y_mm : %d",
+          msg.wp_valid, msg.wp_age_ms, msg.leader_x_mm, msg.leader_y_mm);
     
-    decide_mode_step(&msg);
-
-    msg.cpu_time_ms = (uint32_t)(GetTickCount() & 0xFFFFFFFF);
-    printf("mode=%d reason=%d\n", msg.mode, msg.reason);
-    
-
-
+    usleep(50000); // 50ms delay
     }
 
     return 0;
