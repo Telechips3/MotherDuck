ca972dc63b4a609b3070abe8c637f0f529c2d166
ssamKim0908 <mmmek0401@gmail.com>
2026-02-07 21:03:24 +0900

    fix encoder

--- mother/vcp/sources/app.sample/app.base/Team2/source/encoder.c
+++ mother/vcp/sources/app.sample/app.base/Team2/source/encoder.c
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 #include "../include/encoder.h"
 #include "../team2_header.h"
-#include <gpio.h>     
-#include <bsp.h> 
+#include <gpio.h>
+#include <bsp.h>
 #include <debug.h>
 
 /* ===== Internal Variable ===== */
@@ -11,7 +11,6 @@ static uint8_t s_prev_state = 0;
 
 static uint32 encTaskID;
 static uint32 encTaskStk[ENCODER_TASK_STACK_SIZE];
-
 
 static void Encoder_ISR_Handler(void *args)
 {
@@ -24,24 +23,54 @@ static void Encoder_ISR_Handler(void *args)
     uint8_t state_transition = (s_prev_state << 2) | cur_state;
 
     // 4. 상태 변화에 따른 카운트 (4체배 로직)
-    switch (state_transition) {
-        // 정회전 케이스 (00->01, 01->11, 11->10, 10->00)
-        case 0b0001: case 0b0111: case 0b1110: case 0b1000:
-            g_enc_count++;
-            break;
-            
-        // 역회전 케이스 (00->10, 10->11, 11->01, 01->00)
-        case 0b0010: case 0b1011: case 0b1101: case 0b0100:
-            g_enc_count--;
-            break;
+    switch (state_transition)
+    {
+    // 정회전 케이스 (00->01, 01->11, 11->10, 10->00)
+    case 0b0001:
+    case 0b0111:
+    case 0b1110:
+    case 0b1000:
+        g_enc_count++;
+        break;
 
-        default:
-            
-            break;
+    // 역회전 케이스 (00->10, 10->11, 11->01, 01->00)
+    case 0b0010:
+    case 0b1011:
+    case 0b1101:
+    case 0b0100:
+        g_enc_count--;
+        break;
+
+    default:
+        break;
     }
 
-    s_prev_state = cur_state;
-    mcu_printf("encoder Cnt: %d\n", g_enc_count);
+    //
+    switch (state_transition)
+    {
+    // 1. 정회전(CW) 케이스: A가 먼저 변함
+    case 0b0010: // 00 -> 10 (A Rising)
+    case 0b1011: // 10 -> 11 (B Rising - 이때 A는 High!)
+    case 0b1101: // 11 -> 01 (A Falling)
+    case 0b0100: // 01 -> 00 (B Falling)
+        g_enc_count++;
+        break;
+
+    // 2. 역회전(CCW) 케이스: B가 먼저 변함
+    case 0b0001: // 00 -> 01 (B Rising)
+    case 0b0111: // 01 -> 11 (A Rising)
+    case 0b1110: // 11 -> 10 (B Falling)
+    case 0b1000: // 10 -> 00 (A Falling)
+        g_enc_count--;
+        break;
+
+    default:
+        // 변화 없음 또는 노이즈(00->11 등)
+        break;
+
+        s_prev_state = cur_state;
+        mcu_printf("encoder Cnt: %d\n", g_enc_count);
+    }
 }
 
 /* ===== Init ===== */
