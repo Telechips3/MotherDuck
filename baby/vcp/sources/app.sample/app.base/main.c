30cf06c2646a30534910d9a6e08f76b9b2b70d24
0731chae <0731chae@gmail.com>
2026-02-08 19:49:16 +0900

    fuck

--- baby/vcp/sources/app.sample/app.base/main.c
+++ baby/vcp/sources/app.sample/app.base/main.c
@@ -32,13 +32,13 @@
 #include "sensor_task.h"
 #include "follow_steer_module.h"
 
-#define ENABLE_IPC_TEST             1
+#define ENABLE_IPC_TEST             0
 #define ENABLE_SPI_TEST             1          
 #define ENABLE_IMU_TASK             0       
 #define ENABLE_ENCODER_TASK         0
 #define ENABLE_ULTRASONIC_TASK      0   
 #define ENABLE_POSE_TASK            0
-#define ENABLE_SENSOR_TASK          0
+#define ENABLE_SENSOR_TASK          1
 #define ENABLE_FOLLOW_STEER_TASK    0
 
 #if (APLT_LINUX_SUPPORT_SPI_DEMO == 1)
