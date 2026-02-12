a8dfb7d0a3cf341815f4f93518274ce21acd66f6
zoozeroo <yygs315@naver.com>
2026-02-12 17:35:40 +0900

    upload ai model

---
+++ mother/ai/ai_model/stop_sign_1_quantized/oact_entry_table.py
@@ -0,0 +1,8 @@
+
+oacts = list()
+oacts.append([       0x0,    0x64000,  4,         64,          0, 'Conv2d_57'])
+oacts.append([   0x64000,    0x32000,  4,          1,          0, 'Conv2d_58'])
+oacts.append([   0x96000,    0x19000,  4,         64,          0, 'Conv2d_70'])
+oacts.append([   0xaf000,     0xc800,  4,          1,          0, 'Conv2d_71'])
+oacts.append([   0xbb800,     0x6400,  4,         64,          0, 'Conv2d_81'])
+oacts.append([   0xc1c00,     0x3200,  4,          1,          0, 'Conv2d_82'])
